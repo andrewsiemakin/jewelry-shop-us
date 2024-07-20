2f7ea746ff0167aa5a094769fe41b10b9a73340e
Vladyslav Hryhorovych <vladwwe24@gmail.com>
2024-07-20 12:18:39 -0700

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    trailingSlash: false,
+};
 
 export default nextConfig;

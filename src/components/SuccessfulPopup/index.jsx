4eeb96c8de6643e27c30fdadcf69c104e0781e91
Vladyslav Hryhorovych <vladwwe24@gmail.com>
2024-07-20 01:16:30 -0700

    save

--- src/components/SuccessfulPopup/index.jsx
+++ src/components/SuccessfulPopup/index.jsx
@@ -1,14 +1,8 @@
 import s from "./SuccessfulPopup.module.scss"
 
-import {Noto_Serif_JP} from "next/font/google";
-import {Montserrat} from "next/font/google";
-
 import Portal from "@/components/Portal";
 
 import { LuLoader2 } from "react-icons/lu";
-
-const noto = Noto_Serif_JP({ weight: "600", subsets: ["latin"] });
-const montserrat = Montserrat({ weight: "400", subsets: ["latin"] });
 
 const SuccessfulPopup = ({open, loading = false, onClose}) => {
 
@@ -19,8 +13,8 @@ const SuccessfulPopup = ({open, loading = false, onClose}) => {
                     loading ?
                     <LuLoader2 className={s.loader}/> :
                         <>
-                            <h2 className={[noto.className, s.title].join(" ")}>Everything was successful</h2>
-                            <p className={[montserrat.className, s.description].join(" ")}>We will contact you as soon
+                            <h2 className={[s.title].join(" ")}>Everything was successful</h2>
+                            <p className={[s.description].join(" ")}>We will contact you as soon
                                 as possible</p>
                             <button className={s.btn} onClick={onClose}>OK</button>
                         </>
